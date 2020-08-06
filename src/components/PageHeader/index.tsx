97bbcd73e1a3e146ab0d9f6a6a45d786068f2d36
anakettl <anakettl@gmail.com>
2020-08-05 22:39:58 -0300

    feat(html/css): Add css to Page Header

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css'
+
 interface PageHeaderProps {
   title: string;
 }
@@ -11,17 +13,18 @@ interface PageHeaderProps {
 const PageHeader: React.FC<PageHeaderProps> = (props) => {
   return (
     <header className="page-header">
-        <div className="top-bar-container">
-          <Link to="/">
-            <img src={backIcon} alt="Voltar"/>
-          </Link>
-          <img src={logoImg} alt="Proffy"/>
-        </div>
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="Voltar"/>
+        </Link>
+        <img src={logoImg} alt="Proffy"/>
+      </div>
 
-        <div className="header-content">
-          <strong>{props.title}</strong>
-        </div>
-      </header>
+      <div className="header-content">
+        <strong>{props.title}</strong>
+      </div>
+      {props.children}
+    </header>
   );
 }
 
